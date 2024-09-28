de3da8293d19db5efc81ca603b1b3d175d2c778f
Matias Salazar <matias39974593@gmail.com>
2024-09-28 15:04:47 -0300

    agregamos el middleware para configurar un archivo estatico

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-import express from 'express'
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //1- configurar un puerto
 const app = express();
 
 app.set("port", process.env.PORT || 4000);
-app.listen(app.get("port"), ()=>{
-    console.info("Estoy escuchando el puerto "+ app.get("port"))
-})
+app.listen(app.get("port"), () => {
+  console.info("Estoy escuchando el puerto " + app.get("port"));
+});
 
 //2- configurar middlewares
 app.use(cors()); //permite conexiones remotas
-app.use(morgan('dev')); // nos da informacion extra en la terminal
+app.use(morgan("dev")); // nos da informacion extra en la terminal
 app.use(express.json()); // interpreta los datos del body en el formato json
-app.use(express.urlencoded({extended:true})); // interpretar datos enviados de formularios
-
-// agregar como recibir conexiones remotas
-
+app.use(express.urlencoded({ extended: true })); // interpretar datos enviados de formularios
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//console.log(__filename)
+//console.log(__dirname)
+//console.log(path.join(__dirname,"/public"))
+app.use(express.static(path.join(__dirname, "/public"))); //configuramos un archivo estatico para ver el index.html en la ruta prinipal
 
 //3- configurar las rutas
-app.get("/prueba", (req, res)=>{
-    //console.log("desde la funcion de prueba")
-    res.send("Desde el backend del proyecto crudCafe")
-})
+app.get("/prueba", (req, res) => {
+  //console.log("desde la funcion de prueba")
+  res.send("Desde el backend del proyecto crudCafe");
+});
