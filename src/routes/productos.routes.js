fc18d58b9d7119ce44a9b6ec2ef6711d916bd36b
Matias Salazar <matias39974593@gmail.com>
2024-09-30 17:40:50 -0300

    agregamos la logica para crear un producto y contestamos con error cuando el producto no pudo ser generado

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { leerPrueba } from "../controllers/producto.controllers.js";
+import { crearProducto, leerPrueba } from "../controllers/producto.controllers.js";
 
 //app.get("/prueba", (req, res) => {
 //res.send("Desde el backend del proyecto crudCafe");
 // });
 const router = Router();
 router.route("/prueba").get(leerPrueba);
+router.route("/producto").post(crearProducto);
 
 export default router; 
